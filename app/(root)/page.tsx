6f526eaed3a01225a17d2dfc1d6515de230eb312
rexconsul <reginrexconsul@gmail.com>
2024-12-22 17:49:22 +0800

    startup card functionality, added placeholder data, configure next config for images, added formatDate util

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,50 @@
-import SearchForm from "@/components/SearchForm";
+import SearchForm from '@/components/SearchForm';
+import StartUpCard from '@/components/StartUpCard';
 
-export default async function Home({ searchParams }: {
-  searchParams: Promise<{ query: string;}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'John Doe' },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://plus.unsplash.com/premium_photo-1694819488591-a43907d1c5cc?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      category: 'Dogs',
+      title: 'We are Dogs',
+    },
+  ];
 
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
         <p className="sub-header !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartUpCardType) => (
+              <StartUpCard key={post?._id} post={post} />
+            ))
+          ): (<p className='no-results'>No startups found</p>)}
+        </ul>
       </section>
     </>
   );
