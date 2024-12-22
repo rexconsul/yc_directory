4c11ee190b9a41ce62add2ec613a7ee119c851da
rexconsul <reginrexconsul@gmail.com>
2024-12-22 17:04:56 +0800

    implemented search forms for query, install button component from shadcn, refactor components folder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query: string;}>
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -6,6 +12,7 @@ export default function Home() {
         <p className="sub-header !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
